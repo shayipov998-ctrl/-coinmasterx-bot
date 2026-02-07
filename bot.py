a8907b6ebf283f4283404c7126cd81f2ad58053e
shayipov998-ctrl <shayipov998@icloud.com>
2026-02-08 00:22:40 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,23 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("🚀 CoinMasterX ishga tushdi!")
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Bu crypto signal bot. Tez orada signal keladi.")
+
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("help", help_command))
+
+    print("Bot ishga tushdi...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
